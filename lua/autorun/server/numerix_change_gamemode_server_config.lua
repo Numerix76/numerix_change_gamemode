e9eac2235fb3eacefe20f1736becc52d87adb8fe
Numerix <42088870+Numerix76@users.noreply.github.com>
2022-12-14 17:42:24 +0100

    Create numerix_change_gamemode_server_config.lua

---
+++ lua/autorun/server/numerix_change_gamemode_server_config.lua
@@ -0,0 +1,14 @@
+GamemodeChange = GamemodeChange or {}
+GamemodeChange.Config = GamemodeChange.Config or {}
+
+--Client ID mTxServ (https://mtxserv.com/fr/mon-compte/oauth
+GamemodeChange.Config.client_id = ""
+
+--Client Secret mTxServ (https://mtxserv.com/fr/mon-compte/oauth)
+GamemodeChange.Config.client_secret = ""
+
+--API Key mTxServ (https://mtxserv.com/fr/mon-compte/api)
+GamemodeChange.Config.api_key = ""
+
+--ServerID mTxServ (nombre après le # sous l'adresse IP. Ex : GMod 8 slots #148713 le code sera 148713)
+GamemodeChange.Config.ServerID = ""
