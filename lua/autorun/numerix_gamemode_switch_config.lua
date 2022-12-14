8d06166fdffa215397e7a6799a83768fa5598119
Numerix <42088870+Numerix76@users.noreply.github.com>
2022-12-14 17:42:40 +0100

    Update numerix_gamemode_switch_config.lua

--- lua/autorun/numerix_gamemode_switch_config.lua
+++ lua/autorun/numerix_gamemode_switch_config.lua
@@ -1,18 +1,6 @@
 GamemodeChange = GamemodeChange or {}
 GamemodeChange.Config = GamemodeChange.Config or {}
 
---Client ID mTxServ (https://mtxserv.com/fr/mon-compte/oauth
-GamemodeChange.Config.client_id = ""
-
---Client Secret mTxServ (https://mtxserv.com/fr/mon-compte/oauth)
-GamemodeChange.Config.client_secret = ""
-
---API Key mTxServ (https://mtxserv.com/fr/mon-compte/api)
-GamemodeChange.Config.api_key = ""
-
---ServerID mTxServ (nombre après le # sous l'adresse IP. Ex : GMod 8 slots #148713 le code sera 148713)
-GamemodeChange.Config.ServerID = ""
-
 --Message sur le menu d'attente
 GamemodeChange.Config.textmenu = "Le serveur change de gamemode. Veuillez patientez."
  
